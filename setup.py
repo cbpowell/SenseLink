d99de7f67c038a80f98960909c1030000f2d0c42
Charles Powell <cbpowell@gmail.com>
2022-03-13 15:48:36 -0700

    Update setup.py version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='SenseLink',
-    version='1.4.0',
+    version='2.0.0',
     description='A tool to create virtual smart plugs and inform a Sense Home Energy Monitor about usage in your home',
     long_description=long_description,
     long_description_content_type="text/markdown",
